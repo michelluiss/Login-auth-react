355b779a2c3d429d5c2491d32f4062bf3992440b
michelluiss <michelluis88@gmail.com>
2020-11-16 20:53:00 -0300

    adicionando bootstrap, configurando estrutura de rotas

---
+++ src/routes/routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Switch, Route } from 'react-router-dom';
+import Login from '../views/login/Login';
+import Home from '../views/home/Home';
+
+const Routes = () => (
+  <Switch>
+    <Route exact path="/" component={Login} />
+    <Route path="/home" component={Home} />
+  </Switch>
+)
+
+export default Routes;
---
+++ src/views/login/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => (
+  <div className="login-page">
+    <h1>Login</h1>
+  </div>
+)
+
+export default Login;
